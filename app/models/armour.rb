7b95ffb3ecb79aab02731e5a0af3d5d043d89ab2
dwandrew <danwandrew@gmail.com>
2021-09-29 17:03:23 +0100

    added armour realtionship with lists

--- app/models/armour.rb
+++ app/models/armour.rb
@@ -1,4 +1,6 @@
 class Armour < ApplicationRecord
     has_many :armour_special_rules
     has_many :special_rules, through: :armour_special_rules
+    has_many :equipment_list_armours
+    has_many :equipment_lists, through: :equipment_list_armours
 end
